2d2cc69bc99c7ee60468a729715800dd0007d2e4
Josh Stroud <68628946+XOneTrick@users.noreply.github.com>
2022-03-22 12:42:07 +1300

    item page ccs

--- src/containers/Marketplace/itemPage.tsx
+++ src/containers/Marketplace/itemPage.tsx
@@ -15,7 +15,7 @@ import "../../containers/Home/fonts.css";
 import { height, textAlign } from "@mui/system";
 import IconButton from "@mui/material/IconButton";
 import Navbar from "../Shared/Navbar";
-import { Typography } from "@mui/material";
+import { CardActionArea, Grid, List, ListItem, Typography, Card, CardMedia, CardContent, CardActions, Button } from "@mui/material";
 import profilePlaceholder from "../../images/profilePlaceholder.png";
 import app from "../../base";
 import { getDocs } from "firebase/firestore";
@@ -35,7 +35,42 @@ const useStyles = makeStyles((theme) =>
             maxHeight: "1200",
             display: "flex",
             flexDirection: "row",
+            flexGrowing: 1,
+
         },
+        content: {
+            display: "flex",
+            flexDirection: "column",
+            //alignItems: "center",
+            //justifyContent: "center",
+            width: "100%",
+            height: "100%",
+            //margin: "auto",
+            marginTop: "60px",
+
+        },
+
+        information: {
+            backgroundColor: "#F7E",
+            height: "70%",
+            width: "50%",
+            float: "right",
+
+
+        },
+        photo: {
+            //float: "left",
+            backgroundColor: "#FFF",
+            height: "60%",
+            width: "50%",
+            float: "left",
+        },
+        card: {
+            //minHeight: "500",
+            //minWidth: "500",
+            width: "100%",
+            height: "100%",
+        }
     }),
 );
 
@@ -46,23 +81,23 @@ function ItemPage() {
     const history = useHistory();
 
 
-    const [item, setItem] = useState<any>([]);
+    const [itemData, setItemData] = useState<any>([]);
 
     const getItemData = async () => {
-      const firestore = app.firestore();
-  
-      const collectionRef = firestore.collection("productData");
-      const querySnapshot = await getDocs(collectionRef);
-  
-      querySnapshot.forEach((doc) => {
-        setItem((arr: any) => [...arr, doc.data()]);
-      });
+        const firestore = app.firestore();
+
+        const collectionRef = firestore.collection("productData");
+        const querySnapshot = await getDocs(collectionRef);
+
+        querySnapshot.forEach((doc) => {
+            setItemData((arr: any) => [...arr, doc.data()]);
+        });
     };
-  
+
     useEffect(() => {
-      getItemData();
+        getItemData();
     }, []);
-  
+
     return (
         <>
             {/* Header Container */}
@@ -71,6 +106,115 @@ function ItemPage() {
                 maxWidth={false}
                 className={`${classes.container}`}
             >
+                <Navbar />
+
+                <Container className={`${classes.content}`}>
+
+                    <div className={`${classes.information}`}>
+                        <List>
+                            {itemData.map((item: {
+                                itemTitle: string | undefined;
+                                itemDescription: string | undefined;
+                                price: number | undefined;
+                                itemStock: number | undefined;
+                                photoURL: string | undefined;
+                            }) => (
+                                <ListItem key={item.itemTitle}
+                                >
+                                    <Card
+                                        className={classes.card}
+                                        sx={{ borderRadius: 5 }}
+                                    >
+                                        <CardActionArea
+                                            sx={{ display: "column", border: `5px solid white` }}
+                                        >
+
+                                            <CardContent sx={{ flexDirection: "row" }}>
+                                                <Grid container justifyContent="space-between">
+                                                    <Typography
+                                                        gutterBottom
+                                                        variant="h5"
+                                                        component="div"
+                                                        color="#AC5435"
+                                                    >
+                                                        {item.itemTitle}
+                                                    </Typography>
+                                                </Grid>
+                                                <Typography variant="body2" color="#AC5435">
+                                                    {item.itemDescription}
+                                                </Typography>
+                                                <br />
+                                                <Typography
+                                                    variant="body2"
+                                                    color="#AC5435"
+                                                    align="left"
+                                                >
+                                                    Price: ${item.price}
+                                                </Typography>
+                                                <CardActions sx={{ justifyContent: "end" }}>
+                                                    <Button
+                                                        size="small"
+                                                        color="secondary"
+                                                        variant="contained"
+                                                        sx={{ borderRadius: 25, maxHeight: 25 }}
+                                                    >
+                                                        BUY
+                                                    </Button>
+                                                </CardActions>
+                                            </CardContent>
+                                        </CardActionArea>
+                                    </Card>
+                                </ListItem>
+
+                            ))}
+                        </List>
+                    </div>
+
+                    <div className={`${classes.photo}`}>
+                        <List
+                            sx={{ columns: 1, gap: 3 }}>
+                            {itemData.map(
+                                (item: {
+                                    itemTitle: string | undefined;
+                                    itemDescription: string | undefined;
+                                    photoURL: string | undefined;
+                                    //itemStock: number | undefined;
+                                    price: number | undefined;
+                                }) => (
+                                    <ListItem key={item.itemTitle}
+                                    >
+                                        <Card
+                                            className={classes.card}
+                                            sx={{ borderRadius: 5 }}
+                                        >
+                                            <CardActionArea
+                                                sx={{ display: "column", border: `5px solid white` }}
+                                            >
+                                                <CardMedia
+                                                    component="img"
+                                                    height="100%"
+                                                    width="100%"
+                                                    sx={{ borderRadius: 5 }}
+                                                    image={item.photoURL}
+                                                    alt="No Image Available"
+                                                />
+
+
+
+
+
+                                            </CardActionArea>
+                                        </Card>
+                                    </ListItem>
+                                )
+                            )}
+                        </List>
+
+                    </div>
+
+                </Container>
+
+
 
                 <Footer />
             </Container>
