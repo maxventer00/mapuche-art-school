57b50d4d6c6addf0f20b01c7277ada87a14a3e2d
Max.venter00 <max.venter00@gmail.com>
2022-05-03 16:30:46 +1200

    Fixed wills fuckup

--- src/containers/Home/home.tsx
+++ src/containers/Home/home.tsx
@@ -163,7 +163,7 @@ function Home() {
           remaining essentially unchanged. It was popularised in the 1960s with
           the release of Letraset sheets containing Lorem Ipsum passages, and
           more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
+          including versions of Lorem Ipsum. Yup
         </span>
 
         {loggedIn ? null : (
