1b567d630252892fdfcf5cd01b07e622421e60c5
Max.venter00 <max.venter00@gmail.com>
2022-05-05 11:12:00 +1200

    Installed googlemaps react

--- src/containers/NagcheRoute/customMap.tsx
+++ src/containers/NagcheRoute/customMap.tsx
@@ -1,39 +1,37 @@
-import { GoogleApiWrapper } from 'google-maps-react';
-import {Map, Marker } from 'google-maps-react';
-import {} from './mapStyles.json';
-const mapStyle = './mapStyles.json';
+import { GoogleApiWrapper } from "google-maps-react";
+import { Map, Marker } from "google-maps-react";
+import {} from "./mapStyles.json";
+const mapStyle = "./mapStyles.json";
 
 function CustomMap({ google, locations = [] }: any) {
-    
-
-    return (
-        
-      <Map
-          google={google}
-          containerStyle={{
-              position: "static",
-              width: "100%",
-              height: "100"
-          }}
-          style={{
-              style: mapStyle,
-              width: "94.5%",
-              height: "95%",
-              "margin-left": "2.5%",
-              "margin-top": "13%"
-          }}
-          initialCenter={{
-            lat: -38.2555834745345,
-            lng: -72.88456333399817
-          }}
-          zoom={locations.length = 10}
-      >
-          {locations.map((coords: any) => <Marker position={coords} />
-          )}
-      </Map>
-  )
+  return (
+    <Map
+      google={google}
+      containerStyle={{
+        position: "static",
+        width: "100%",
+        height: "100",
+      }}
+      style={{
+        style: mapStyle,
+        width: "94.5%",
+        height: "95%",
+        "margin-left": "2.5%",
+        "margin-top": "13%",
+      }}
+      initialCenter={{
+        lat: -38.2555834745345,
+        lng: -72.88456333399817,
+      }}
+      zoom={(locations.length = 10)}
+    >
+      {locations.map((coords: any) => (
+        <Marker position={coords} />
+      ))}
+    </Map>
+  );
 }
 
 export default GoogleApiWrapper({
-    apiKey: "AIzaSyAGTjefET42RORseiTh5HgIYOq_oXaZgJY",
+  apiKey: "AIzaSyAGTjefET42RORseiTh5HgIYOq_oXaZgJY",
 })(CustomMap);
