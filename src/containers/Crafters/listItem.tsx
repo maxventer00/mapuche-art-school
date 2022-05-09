3654b78b283c4e305d3c42fa8d37df7cadf6582f
Josh Stroud <68628946+XOneTrick@users.noreply.github.com>
2022-05-09 19:51:41 +1200

    List Item page styling part 1

--- src/containers/Crafters/listItem.tsx
+++ src/containers/Crafters/listItem.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { Button, Container, createTheme, TextField } from "@mui/material";
+import { Box, Button, Container, createTheme, Grid, Paper, TextField } from "@mui/material";
 import { useHistory } from "react-router";
 import Footer from "../Shared/footer";
 import { createStyles, makeStyles } from "@mui/styles";
+import { styled } from '@mui/material/styles';
 import {
   getDownloadURL,
   ref,
@@ -50,22 +51,6 @@ const useStyles = makeStyles((theme) =>
       display: "flex",
       flexDirection: "row",
     },
-
-    footerContainer: {
-      backgroundColor: "#F7ECE1",
-      position: "absolute",
-      left: 0,
-      bottom: 0,
-      right: 0,
-    },
-    textFld: {
-      width: 500,
-      height: 60,
-      display: "column",
-      //marginRight: 'auto',
-      marginBottom: 30,
-      fontSize: 35,
-    },
     input: {
       color: "#FFFFFF",
       fontSize: 25,
@@ -78,63 +63,58 @@ const useStyles = makeStyles((theme) =>
       height: 200,
       fontSize: "3em",
     },
-    formContainer: {
+    boxes: {
+      marginBottom: "1%",
+      marginTop: "150px",
+      margin: "auto",
+      width: "80%",
       display: "flex",
-      flexDirection: "row",
+    },
+    textFld: {
       width: "100%",
-      margin: "auto",
-      //marginTop: "60px",
-    },
-    photoContainer: {
-      maxWidth: "400px",
-      maxHeight: "600px",
-      margin: "auto",
-      marginLeft: "10%",
-      borderWidth: "2px",
-      display: "flex",
-      flex: 1,
-      marginTop: "60px",
-    },
-    fieldContainer: {
+      height: 60,
       display: "column",
-      marginLeft: "auto",
-      paddingBottom: 80,
-      alignItems: "center",
-      paddingTop: 80,
-      flex: 1,
+      margin: "auto",
+      fontSize: 35,
     },
-    descriptionContainer: {
-      display: "column",
-      marginLeft: "auto",
-      paddingBottom: 80,
-      alignItems: "center",
-      paddingTop: 80,
-      flex: 1,
-    },
-    profileConatiner: {
-      maxWidth: 400,
-      display: "flex",
-      marginLeft: "auto",
-      marginRight: "auto",
-      paddingTop: 70,
-    },
+    pictures: {
+      backgroundPosition: "center",
+      backgroundRepeat: "no-repeat",
+      height: 600,
+      width: "75%",
+      textAlign: "center",
+      margin: "auto",
 
-    buttonsContainer: {
-      display: "flex",
-      flexDirection: "row",
-      width: "50%",
-      margin: "auto",
     },
-    photoButtons: {
-      display: "flex",
-      flexDirection: "row",
+    inputs: {
+      backgroundPosition: "center",
+      backgroundRepeat: "no-repeat",
+      height: "57%",
+      width: "100%",
+      textAlign: "center",
+      marginLeft: "auto",
+      backgroundColor: "transparent",
+    },
+    buttons: {
+      backgroundPosition: "center",
+      backgroundRepeat: "no-repeat",
+      height: "7%",
       width: "50%",
+      textAlign: "center",
       margin: "auto",
-      paddingLeft: "12%",
-      paddingTop: "5%",
+      marginTop: "110px",
+      backgroundColor: "transparent",
     },
   })
 );
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#b8a088',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 const theme = createTheme();
 
@@ -278,160 +258,168 @@ function ListItem() {
 
   return (
     <>
-      <Navbar />
       <Container
         disableGutters
         maxWidth={false}
         className={`${classes.container}`}
       >
-        <div className={classes.formContainer}>
-          {photoURLs ? (
-            <div className={classes.photoContainer}>
-              <img
-                src={photoURLs[0]}
-                style={{
-                  maxHeight: "100%",
-                  maxWidth: "100%",
-                }}
-              />
-            </div>
-          ) : (
-            <div className={classes.photoContainer}>
-              <img src={profilePlaceholder} />
-            </div>
-          )}
-          <div className={classes.fieldContainer}>
-            <TextField
-              id="ItemTitle"
-              className={classes.textFld}
-              label="Enter Item's Title"
-              value={itemTitle}
-              variant="filled"
-              InputProps={{
-                classes: {
-                  input: classes.input,
-                },
-              }}
-              InputLabelProps={{
-                classes: {
-                  root: classes.label,
-                },
-              }}
-              onChange={(e) => setItemTitle(e.target.value)}
-            />
+        <Navbar />
 
-            <TextField
-              id="Price"
-              className={classes.textFld}
-              label="Enter a price"
-              variant="filled"
-              value={price}
-              InputProps={{
-                classes: {
-                  input: classes.label,
-                },
-              }}
-              InputLabelProps={{
-                classes: {
-                  root: classes.label,
-                },
-              }}
-              onChange={(e) => setPrice(e.target.value)}
-            />
+        <Box sx={{ flexGrow: 1 }} className={`${classes.boxes}`}>
+          <Grid container spacing={5}>
+            <Grid item xs={6} md={7}>
+              <Item>
 
-            <TextField
-              id="Stock"
-              className={classes.textFld}
-              label="Enter amount of Stock"
-              variant="filled"
-              value={itemStock}
-              InputProps={{
-                classes: {
-                  input: classes.label,
-                },
-              }}
-              InputLabelProps={{
-                classes: {
-                  root: classes.label,
-                },
-              }}
-              onChange={(e) => setItemStock(e.target.value)}
-            />
-          </div>
-          <div className={classes.descriptionContainer}>
-            <TextField
-              id="ItemDescription"
-              className={classes.textFld}
-              multiline
-              maxRows={6}
-              rows={6}
-              label="Add an Item Description"
-              value={itemDescription}
-              variant="filled"
-              InputProps={{
-                classes: {
-                  input: classes.input2,
-                },
-              }}
-              InputLabelProps={{
-                classes: {
-                  root: classes.label,
-                },
-              }}
-              onChange={(e) => setItemDescription(e.target.value)}
-            />
-          </div>
-        </div>
+                <Paper square={true} elevation={24} className={`${classes.pictures}`}>
+                  <img
+                    src={photoURLs[0]}
+                    style={{
+                      maxHeight: 600,
+                      maxWidth: "50%",
+                      marginTop: "10%",
+                    }}
+                  />
+                </Paper>
 
-        <Container justify-content="center" align-items="center">
-          <div className={classes.photoButtons}>
-            <Button
-              variant="contained"
-              component="label"
-              style={{
-                marginRight: "20px",
-                maxWidth: "60%",
-                paddingLeft: "20px",
-              }}
-            >
-              #Browse File
-              <input
-                type="file"
-                multiple
-                hidden
-                accept="image/png, image/jpeg, image/jpg"
-                onChange={(e) => {
-                  if (e.target.files !== null) {
-                    if (e.target.files.length > 0) {
-                      const tempArray = [];
-                      for (var i = 0; i < e.target.files.length; i++) {
-                        tempArray.push(e.target.files[i]);
-                        console.log("Added image " + e.target.files[i].name);
+
+
+              </Item>
+            </Grid>
+            <Grid item xs={6} md={5}>
+              <Item className={`${classes.inputs}`}>
+                <TextField
+                  id="ItemTitle"
+                  className={classes.textFld}
+                  label="Enter Item's Title"
+                  value={itemTitle}
+                  variant="filled"
+                  InputProps={{
+                    classes: {
+                      input: classes.input,
+                    },
+                  }}
+                  InputLabelProps={{
+                    classes: {
+                      root: classes.label,
+                    },
+                  }}
+                  onChange={(e) => setItemTitle(e.target.value)}
+                />
+
+                <TextField
+                  id="Price"
+                  className={classes.textFld}
+                  label="Enter a price"
+                  variant="filled"
+                  value={price}
+                  InputProps={{
+                    classes: {
+                      input: classes.label,
+                    },
+                  }}
+                  InputLabelProps={{
+                    classes: {
+                      root: classes.label,
+                    },
+                  }}
+                  onChange={(e) => setPrice(e.target.value)}
+                />
+
+                <TextField
+                  id="Stock"
+                  className={classes.textFld}
+                  label="Enter amount of Stock"
+                  variant="filled"
+                  value={itemStock}
+                  InputProps={{
+                    classes: {
+                      input: classes.label,
+                    },
+                  }}
+                  InputLabelProps={{
+                    classes: {
+                      root: classes.label,
+                    },
+                  }}
+                  onChange={(e) => setItemStock(e.target.value)}
+                />
+                <TextField
+                  id="ItemDescription"
+                  className={classes.textFld}
+                  multiline
+                  maxRows={6}
+                  rows={6}
+                  label="Add an Item Description"
+                  value={itemDescription}
+                  variant="filled"
+                  InputProps={{
+                    classes: {
+                      input: classes.input2,
+                    },
+                  }}
+                  InputLabelProps={{
+                    classes: {
+                      root: classes.label,
+                    },
+                  }}
+                  onChange={(e) => setItemDescription(e.target.value)}
+                />
+
+              </Item>
+              <Item className={`${classes.buttons}`}>
+                <Button
+                  size="large"
+                  variant="contained"
+                  component="label"
+                  style={{
+                    marginRight: "20px",
+                    maxWidth: "50%",
+                  }}
+                >
+                  #Browse File
+                  <input
+                    type="file"
+                    multiple
+                    hidden
+                    accept="image/png, image/jpeg, image/jpg"
+                    onChange={(e) => {
+                      if (e.target.files !== null) {
+                        if (e.target.files.length > 0) {
+                          const tempArray = [];
+                          for (var i = 0; i < e.target.files.length; i++) {
+                            tempArray.push(e.target.files[i]);
+                            console.log("Added image " + e.target.files[i].name);
+                          }
+                          setImages(tempArray);
+                        }
                       }
-                      setImages(tempArray);
-                    }
-                  }
-                }}
-              />
-            </Button>
+                    }}
+                  />
+                </Button>
 
-            {listButton ? (
-              <Button
-                size="large"
-                variant="contained"
-                style={{
-                  marginRight: "20px",
-                  maxWidth: "60%",
-                }}
-                onClick={() => ListItemToFireBase()}
-              >
-                List Item
-              </Button>
-            ) : null}
-          </div>
-        </Container>
+                {listButton ? (
+                  <Button
+                    size="large"
+                    variant="contained"
+                    style={{
+                      
+                      maxWidth: "50%",
+                      
+                    }}
+                    onClick={() => ListItemToFireBase()}
+                  >
+                    List Item
+                  </Button>
+                ) : null}
 
-        <Footer />
+              </Item>
+            </Grid>
+
+
+
+          </Grid>
+        </Box>
+
       </Container>
     </>
   );
--- src/containers/MapucheHistory/mapucheHistory.tsx
+++ src/containers/MapucheHistory/mapucheHistory.tsx
@@ -5,7 +5,7 @@ import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import "../../containers/Home/fonts.css";
 import Navbar from "../Shared/Navbar";
-import {Button} from "@mui/material";
+import { Button } from "@mui/material";
 import app from "../../base";
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
@@ -73,7 +73,7 @@ const useStyles = makeStyles((theme) =>
       fontSize: 48,
       color: "#AC5435",
       fontFamily: "Prata",
-      
+
     },
     description: {
       color: "#ffffff",
@@ -267,68 +267,68 @@ function MapucheHistory() {
       >
         <Navbar />
         <Parallax bgImage={salto} strength={600}>
-        {loggedIn ? (
-          <div className={classes.signoutButtonContainer}>
-            <Button
-              style={{ maxWidth: "155px" }}
-              className={classes.outlined}
-              variant="outlined"
-              onClick={() => signOut()}
-            >
-              Sign Out
-            </Button>
-          </div>
-        ) : null}
+          {loggedIn ? (
+            <div className={classes.signoutButtonContainer}>
+              <Button
+                style={{ maxWidth: "155px" }}
+                className={classes.outlined}
+                variant="outlined"
+                onClick={() => signOut()}
+              >
+                Sign Out
+              </Button>
+            </div>
+          ) : null}
 
-        <h1 className={`${classes.h1_header}`}>Mapuche History</h1>
-        <span className={`${classes.description}`}>
-        The Mapuche (/mæˈpʊtʃi/ (Mapuche & Spanish: [maˈputʃe])) are a group of indigenous inhabitants of present-day south-central Chile and southwestern Argentina, including parts of present-day Patagonia. The collective term refers to a wide-ranging ethnicity composed of various groups who shared a common social, religious, and economic structure, as well as a common linguistic heritage as Mapudungun speakers. Their influence once extended from Aconcagua Valley to Chiloé Archipelago and later spread eastward to Puelmapu, a land comprising part of the Argentine pampa and Patagonia. Today the collective group makes up over 80% of the indigenous peoples in Chile, and about 9% of the total Chilean population. The Mapuche are particularly concentrated in the Araucanía region. Many have migrated from rural areas to the cities of Santiago and Buenos Aires for economic opportunities.
-        </span>
-         </Parallax>
-         <Box sx={{ flexGrow: 1 }} className={`${classes.boxes}`}>
-      <Grid container spacing={2}>
-        <Grid item xs={6} md={8}>
-          <Item>
-          <h1 className={`${classes.pictureheader}`}>Lautaro</h1>
-        <Paper square={true} elevation={24} className={`${classes.pictures}`}>
-        </Paper>
-        <span className={`${classes.dummytext}`}>
-        Lautaro was the son of a Mapuche lonko (a chief who holds office during peacetime) called, Curiñancu (Kurüñamku in the Mapuche language, Mapudungun, ‘aguilucho negro’ meaning ‘black harrier’) and was born in 1533. He lived a normal life until, at the age of 11, he was captured by the Spanish and forced into servitude by Don Pedro de Valdivia and became his personal servant. Since it was difficult for the Spaniards to pronounce Lautaro’s original name, Leftraru, they gave him the name of Felipe Lautaro. Don Pedro de Valdivia was a Spanish conqueror of Chile and then became the captain general of Chile.
-        </span>
-          </Item>
-        </Grid>
-        <Grid item xs={6} md={4}>
-          <Item>
-          <h1 className={`${classes.pictureheader}`}>Etymology</h1>
-        <Paper square={true} elevation={24} className={`${classes.pictures2}`}>
-        </Paper>
-        <span className={`${classes.dummytext2}`}>
-        Nagche: "people of the plains" occupied Nag mapu, "the land of the plains" (located in sectors of the Cordillera de Nahuelbuta and the low zones bordering it). Its epic and literary name is Araucanians and its old autochthonous name is Reche. The ancient Mapuche Toqui ("axe-bearer") like Lef-Traru ("swift hawk", better known as Lautaro), Kallfülikan ("blue quartz stone", better known as Caupolicán – "polished flint") or Pelontraru ("Shining Caracara", better known as Pelantaro) were Nagche.
-        </span>
-          </Item>
-        </Grid>
-        <Grid item xs={6} md={4}>
-          <Item>
-          <h1 className={`${classes.pictureheader}`}>Cosmology and beliefs</h1>
-        <Paper square={true} elevation={24} className={`${classes.pictures4}`}>
-        </Paper>
-        <span className={`${classes.dummytext4}`}>
-        Central to Mapuche cosmology is the idea of a creator called ngenechen, who is embodied in four components: an older man (fucha/futra/cha chau), an older woman (kude/kuse), a young man and a young woman. They believe in worlds known as the Wenu Mapu and Minche Mapu. Also, Mapuche cosmology is informed by complex notions of spirits that coexist with humans and animals in the natural world, and daily circumstances can dictate spiritual practices.
-        </span>
-          </Item>
-        </Grid>
-        <Grid item xs={6} md={8}>
-          <Item>
-          <h1 className={`${classes.pictureheader}`}>Incorporation into Chile and Argentina</h1>
-        <Paper square={true} elevation={24} className={`${classes.pictures3}`}>
-        </Paper>
-        <span className={`${classes.dummytext3}`}>
-        In the nineteenth century Chile experienced a fast territorial expansion. Chile established a colony at the Strait of Magellan in 1843, settled Valdivia, Osorno and Llanquihue with German immigrants and conquered land from Peru and Bolivia. Later Chile would also annex Easter Island. In this context Araucanía began to be conquered by Chile due to two reasons. First, the Chilean state aimed for territorial continuity and second it remained the sole place for Chilean agriculture to expand.
-        </span>
-          </Item>
-        </Grid>
-      </Grid>
-    </Box>
+          <h1 className={`${classes.h1_header}`}>Mapuche History</h1>
+          <span className={`${classes.description}`}>
+            The Mapuche (/mæˈpʊtʃi/ (Mapuche & Spanish: [maˈputʃe])) are a group of indigenous inhabitants of present-day south-central Chile and southwestern Argentina, including parts of present-day Patagonia. The collective term refers to a wide-ranging ethnicity composed of various groups who shared a common social, religious, and economic structure, as well as a common linguistic heritage as Mapudungun speakers. Their influence once extended from Aconcagua Valley to Chiloé Archipelago and later spread eastward to Puelmapu, a land comprising part of the Argentine pampa and Patagonia. Today the collective group makes up over 80% of the indigenous peoples in Chile, and about 9% of the total Chilean population. The Mapuche are particularly concentrated in the Araucanía region. Many have migrated from rural areas to the cities of Santiago and Buenos Aires for economic opportunities.
+          </span>
+        </Parallax>
+        <Box sx={{ flexGrow: 1 }} className={`${classes.boxes}`}>
+          <Grid container spacing={2}>
+            <Grid item xs={6} md={8}>
+              <Item>
+                <h1 className={`${classes.pictureheader}`}>Lautaro</h1>
+                <Paper square={true} elevation={24} className={`${classes.pictures}`}>
+                </Paper>
+                <span className={`${classes.dummytext}`}>
+                  Lautaro was the son of a Mapuche lonko (a chief who holds office during peacetime) called, Curiñancu (Kurüñamku in the Mapuche language, Mapudungun, ‘aguilucho negro’ meaning ‘black harrier’) and was born in 1533. He lived a normal life until, at the age of 11, he was captured by the Spanish and forced into servitude by Don Pedro de Valdivia and became his personal servant. Since it was difficult for the Spaniards to pronounce Lautaro’s original name, Leftraru, they gave him the name of Felipe Lautaro. Don Pedro de Valdivia was a Spanish conqueror of Chile and then became the captain general of Chile.
+                </span>
+              </Item>
+            </Grid>
+            <Grid item xs={6} md={4}>
+              <Item>
+                <h1 className={`${classes.pictureheader}`}>Etymology</h1>
+                <Paper square={true} elevation={24} className={`${classes.pictures2}`}>
+                </Paper>
+                <span className={`${classes.dummytext2}`}>
+                  Nagche: "people of the plains" occupied Nag mapu, "the land of the plains" (located in sectors of the Cordillera de Nahuelbuta and the low zones bordering it). Its epic and literary name is Araucanians and its old autochthonous name is Reche. The ancient Mapuche Toqui ("axe-bearer") like Lef-Traru ("swift hawk", better known as Lautaro), Kallfülikan ("blue quartz stone", better known as Caupolicán – "polished flint") or Pelontraru ("Shining Caracara", better known as Pelantaro) were Nagche.
+                </span>
+              </Item>
+            </Grid>
+            <Grid item xs={6} md={4}>
+              <Item>
+                <h1 className={`${classes.pictureheader}`}>Cosmology and beliefs</h1>
+                <Paper square={true} elevation={24} className={`${classes.pictures4}`}>
+                </Paper>
+                <span className={`${classes.dummytext4}`}>
+                  Central to Mapuche cosmology is the idea of a creator called ngenechen, who is embodied in four components: an older man (fucha/futra/cha chau), an older woman (kude/kuse), a young man and a young woman. They believe in worlds known as the Wenu Mapu and Minche Mapu. Also, Mapuche cosmology is informed by complex notions of spirits that coexist with humans and animals in the natural world, and daily circumstances can dictate spiritual practices.
+                </span>
+              </Item>
+            </Grid>
+            <Grid item xs={6} md={8}>
+              <Item>
+                <h1 className={`${classes.pictureheader}`}>Incorporation into Chile and Argentina</h1>
+                <Paper square={true} elevation={24} className={`${classes.pictures3}`}>
+                </Paper>
+                <span className={`${classes.dummytext3}`}>
+                  In the nineteenth century Chile experienced a fast territorial expansion. Chile established a colony at the Strait of Magellan in 1843, settled Valdivia, Osorno and Llanquihue with German immigrants and conquered land from Peru and Bolivia. Later Chile would also annex Easter Island. In this context Araucanía began to be conquered by Chile due to two reasons. First, the Chilean state aimed for territorial continuity and second it remained the sole place for Chilean agriculture to expand.
+                </span>
+              </Item>
+            </Grid>
+          </Grid>
+        </Box>
       </Container>
     </>
   );
