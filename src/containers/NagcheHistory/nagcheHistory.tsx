58a665c3ce5ee7e6c225f5e757c640204d917894
km5x2 <kenneth.s97@gmail.com>
2022-06-01 23:38:49 +1200

    Final Deployment

--- src/containers/NagcheHistory/nagcheHistory.tsx
+++ src/containers/NagcheHistory/nagcheHistory.tsx
@@ -8,7 +8,7 @@ import { Button} from "@mui/material";
 import app from "../../base";
 import { Parallax } from "react-parallax";
 import cholchol from "../../images/cholchol.jpg";
-import eljoven from "../../images/eljoven.png";
+import mapucheflag from "../../images/mapucheflag.png";
 // Way to add EXTRA css values
 const useStyles = makeStyles((theme) =>
   createStyles({
@@ -124,7 +124,7 @@ const useStyles = makeStyles((theme) =>
       width: "75%",
       textAlign: "center",
       marginLeft: "13%",
-      backgroundImage: "url(" + eljoven + ")",
+      backgroundImage: "url(" + mapucheflag + ")",
     },
     maincontainer: {
       backgroundColor: "#F7ECE1",
@@ -189,24 +189,22 @@ function NagcheHistory() {
           </div>
         ) : null}
 
-        <h1 className={`${classes.h1_header}`}>Nagche History</h1>
-        <span className={`${classes.description}`}>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-        </span>
+        <h1 className={`${classes.h1_header}`}>Nagche</h1>
          </Parallax>
-         <h1 className={`${classes.pictureheader}`}>Lautaro</h1>
-        <Paper square={true} elevation={24} className={`${classes.pictures}`}>
-        </Paper>
         <span className={`${classes.dummytext}`}>
-        Lautaro was the son of a Mapuche lonko (a chief who holds office during peacetime) called, Curiñancu (Kurüñamku in the Mapuche language, Mapudungun, ‘aguilucho negro’ meaning ‘black harrier’) and was born in 1533. He lived a normal life until, at the age of 11, he was captured by the Spanish and forced into servitude by Don Pedro de Valdivia and became his personal servant. Since it was difficult for the Spaniards to pronounce Lautaro’s original name, Leftraru, they gave him the name of Felipe Lautaro. Don Pedro de Valdivia was a Spanish conqueror of Chile and then became the captain general of Chile.
+        <p>Tal como se señaló, el pueblo Mapuche desde tiempos prehispánico, ocupó los diferentes nichos ecológicos del sur de Chile, desde la costa pacífico, la cordillera de los Andes y la pampa argentina. Si bien en su vida de pueblo independiente (que dataremos hasta 1881 con su derrota militar en Tucapel y la fundación de Temuco), el pueblo mapuche se desplazaba por los diferentes espacios descritos, de acuerdo con sus necesidades económicas, de intercambio comercial y la crianza de su ganado caballar. Luego de estos episodios, se establecieron de manera más permanente y forzada, en dichos territorios. </p>
+        <p>Desde el presente, las identidades territoriales del pueblo Mapuche que más se conocen son la Identidad Williche, que cubre el füta willi mapu (la gran tierra del sur) que abarca las actuales regiones de Los Ríos y Los Lagos. La identidad Peweche, que bordea la zona oeste de la cordillera de los Andes, entre las actuales regiones del Bio Bio y la Araucanía. La identidad Wenteche aún ocupa parte del valle central de la región de la Araucanía. Allende de la cordillera de los Andes, tenemos al pueblo Puelche y Tehuelche. En la zona costera del océano Pacífico, de las actuales regiones del Bio Bio y la Araucanía, se ubica la identidad Lafkenche. </p>
+        <p>La identidad territorial Nagche, ocupa el este de la Cordillera de la Costa o Cordillera de Nawelfüta (El gran tigre), entre las actuales regiones del Bio Bio y la Araucanía, concentrándose principalmente en las actuales comunas de Purén, Lumaco, Traiguén, Galvarino y Chol Chol. De ahí deriva el Nombre de la actual Área de Desarrollo Indígena (ADI) Puel Nawelfüta, haciendo referencia al este de esta cordillera (puel: este). Desde el inicio de la conquista española y luego en la guerra con el ejército chileno, este territorio fue la primera línea de resistencia y contención del avance extranjero, lo que caracteriza su espíritu guerrero. Sus montañas, de bosques nativos en sus orígenes, junto a zonas de quebradas y valles internos, permitieron hacer de esta zona un lugar de refugio para los kona (guerrero mapuche) y sus familias, los que fueron utilizadas también para emboscadas y escaramuzas que le aseguraban el éxito en sus acciones, dado su dominio del terreno.</p>
+        <p>En este territorio, se dieron hechos históricos emblemáticos, tales como el triunfo de los mapuche sobre </p>
+        <p>los españoles en Curalaba[1] el año 1598, lo que permitió la realización del Parlamento y Tratado de Quilín (Río Quillen, entre las actuales Comuna de Lautaro y Galvarino) en 1641, lo que entre otros punto, fija la frontera del Bio Bio, dando comienzo a un periodo de relativa calma entre las partes, y que en la historiografía tradicional chilena se conoce como la Colonia. Este tratado fue refrendado por la Corona Española. Algunos de los lonko más conocido de este territorio fueron, por el lado sur de la cordillera, Venancio Coñoepan, en el sector de Repocura (actual comuna de Chol Chol) y Lorenzo Colipi, hacia el norte, en la actual comuna de Purén.</p>
+        <p>Del mismo modo, la zona alberga muchos “renü”, cuevas que conectan diferentes valles internos de la Cordillera de Nawelfüta, los que fueron usado militarmente por los mapuche, como lugares de refugio, o rituales, según las zonas y tiempos históricos. También se pueden distinguir los kuel, especie de cerros (o pirámides, según el arqueólogo Tom Dillehay) en honor a un lonko destacado, que se iban formando tras sucesivas capas de tierras que se acumulaban, traídas de diferentes territorios, en encuentros que se realizaban con relativa frecuencia para recordar a dicha persona. En la Comuna de Galvarino, la toponimia del lugar hace referencia a este hecho, como el conocido cordón montañoso denominado Kuel-Ñielol[2]. </p>
+        <p>Otros lugares que aún pueden identificar las comunidades mapuche de este territorio, son los txen-txen, cerros tutelares para los mapuche, los eltun, cementerio antiguos, luegos prohibidos por el Estado chileno, los ngillatuwe, lugares para hacer las ceremonias religiosas comunitarias (ngillatun), y los paliwe, zonas donde se practica el juego del palin o chueca. En la zona nagche, es habitual que el paliwe sea a la vez el lugar para hacer el ngillatun. </p>
+        <p>En lo que respecta a las características naturales de la zona, y a pesar que en la actualidad se encuentra invadida por las plantaciones de pinos y eucaliptus, lo que ha provocado una degradación del suelo y escasez de agua, aún guarda diferentes saltos de agua o txayenko, como los de Kelen Kelen en Cholchol, salto del Río Quillem en Galvarino, salto de Chufken en Traiguen y Salto Rayen en Puren. Todos estos conforman una rica olla hidrográfica que luego desembocará en el rio Imperial o Txaitxaiko. Hoy dichos cursos de agua se encuentran en peligro por la industria forestal.</p>
+        <p>Al extremo norte de la Cordillera de la Costas, en el Parque Nacional Nahuelbuta (entre las comunas de Lumaco y Angol) aún se puede apreciar la belleza del paisaje original de este territorio, que entre otros atractivo, nos muestra bosque de araucarias y alturas que nos permitirán disfrutar de la vista del valle, y desde la Cordillera de los Andes hasta el océano Pacífico, cuando el clima lo permite.</p>
+        <p>En lo lingüístico, la identidad nagche, conforma una de las variantes dialectales del mapuzugun o lengua mapuche, caracterizado por sus sonidos “b” en vez de “f” y “d” en vez de “z” (ejemplo: büta/füta y domo/zomo). En todo caso, las variaciones dialectales de la lengua mapuche son poco significativas, siendo todas ellas perfectamente inteligibles entre si. De igual modo, en lo cultural se marcan algunas característica, expresado por ejemplo, en aspectos rituales, y la vestimenta, especialmente el txarilonko (joya de plata que se usa en la cabeza) y el küpam (vestido de la mujer mapuche).</p>
+        <p>________________________________________________________________________________________________</p>
+        <p>[1] Cuaralaba, ubicada en la actual Comuna de Lumaco, Provincia de Malleco, Región de la Araucanía.</p>
+        <p>[2] Para alguno ñielol puede referirse al ñizol, autoridad mapuche que lideraba a varios lonko. Para otros la palabra puede venir de ngenh-lol, el espíritu dueño (ngenh) de la cueva (lol).</p>
         </span>
       </Container>
     </>
